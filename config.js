840855dc40d883772072afc8b3539d9a4bd32185
Max <max.w29@hotmail.com>
2018-06-28 06:36:39 -0500

    added .travis.yml

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/blogposts';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blogposts';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://maxemil:abc123@ds121341.mlab.com:21341/blog-challenge-with-ci';
 exports.PORT = process.env.PORT || 8080;
